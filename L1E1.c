afd54f5cb17fafa97fd840e6c749c2c1638b6248
Miranda Marques <137963450+OMirandinha@users.noreply.github.com>
2025-10-26 10:13:49 -0300

    Add files via upload

---
+++ L1E1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    float PrecUnit, QtdeVend, valCompra;
+    scanf("%f", &PrecUnit);
+    scanf("%f", &QtdeVend);
+    valCompra = PrecUnit * QtdeVend;
+    printf("%f", valCompra);
+
+}
