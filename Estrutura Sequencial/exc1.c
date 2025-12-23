46d369447055236a1694c4e41039a3ab986b73b5
Miranda Marques <137963450+OMirandinha@users.noreply.github.com>
2025-12-23 14:09:50 -0300

    Add files via upload

---
+++ Estrutura Sequencial/exc1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	float precUnit, valFinal;
+	int qtdeVendas;
+	scanf("%f", &precUnit);
+	scanf("%i", &qtdeVendas);
+	valFinal = precUnit * qtdeVendas;
+	printf("%f", valFinal);
+	
+}
